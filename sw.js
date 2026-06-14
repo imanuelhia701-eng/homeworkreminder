966b15cfceae39d07461e22e8a791e990bf58a67
imanuelhia701-eng <imanuelhia701@gmail.com>
2026-06-14 20:21:31 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    clients.claim();
+});
+
+self.addEventListener("notificationclick", event => {
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow("./")
+    );
+});
